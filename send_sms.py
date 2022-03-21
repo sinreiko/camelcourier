04bf01eb42b5ad80c54cd9aa543c27c4bf792a6f
Reiko Lee <sinreiko@hotmail.com>
2022-03-21 19:13:16 +0800

    send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -1,5 +1,10 @@
 from twilio.rest import Client
 from flask import Flask
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 app = Flask(__name__)
 
 
@@ -11,9 +16,9 @@ def hello():
 @app.route("/update")
 def sendClientUpdate():
     # Your Account SID from twilio.com/console
-    account_sid = "AC872941965f08ba52b89f8698deae23ab"
+    account_sid = os.getenv('TWILIO_ACCOUNT_SID')
     # Your Auth Token from twilio.com/console
-    auth_token = "c13b675c9e1067ba77d259a1824498d9"
+    auth_token = os.getenv('TWILIO_AUTH_TOKEN')
 
     client = Client(account_sid, auth_token)
 
