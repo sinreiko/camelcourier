2250f0e0be3f742320b066ff08c7e5b1872e7238
Vasn <vasilisngjunquan@gmail.com>
2022-03-24 07:32:40 +0800

    add amqp requirements and create pick parcel service

---
+++ pick_parcel.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+import os, sys
+
+import requests
+from invokes import invoke_http
+
+import amqp_setup
+import pika
+import json
+
+app = Flask(__name__)
+CORS(app)
+
