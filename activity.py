0f49cd88abc65e512f3ecae1766a145cefa45cce
Vasn <vasilisngjunquan@gmail.com>
2022-03-24 05:17:10 +0800

    add activity sql and working activity insert

--- activity.py
+++ activity.py
@@ -1,43 +1,21 @@
 #!/usr/bin/env python3
 # activity log service
 
-from datetime import datetime
+# for amqp
 import amqp_setup
 
+# for database
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+
 
 from datetime import datetime
 from os import environ
 import json
 import os
 
-# #### Receiving Activity Log ####
-monitorBindingKey = "#.order"
-
-def receiveActivity():
-    amqp_setup.check_setup()
-
-    queue_name = "Activity"
-
-    # set up a consumer and start to wait for coming messages
-    amqp_setup.channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
-    amqp_setup.channel.start_consuming() # an implicit loop waiting to receive messages; 
-    #it doesn't exit by default. Use Ctrl+C in the command window to terminate it.
-
-def callback(channel, method, properties, body): # required signature for the callback; no return
-    print("\nReceived an activity log by " + __file__)
-    processActivity(json.loads(body))
-    print() # print a new line feed
-
-def processActivity(activity):
-    print("Recording an order log:")
-    print(activity)
-
-
-#### Getting Activity Log ####
-
+#### Linking to DB ####
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL') or 'mysql+mysqlconnector://root@localhost:3306/camelcourier'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -55,7 +33,7 @@ class Activity(db.Model):
     timestamp = db.Column(db.DateTime, nullable=False, default=datetime.now,        onupdate=datetime.now)
     deliveryStatus = db.Column(db.String(30), nullable=False)
     deliveryDesc = db.Column(db.String(130), nullable=False)
-
+    
     def json(self):
         dto = {
             'activity_id': self.activityID,
@@ -67,26 +45,92 @@ class Activity(db.Model):
 
         return dto
 
-@app.route("/activity/<string:trackingID>")
-def find_activity_by_tracking(trackingID):
-    activityList =  Activity.query.filter_by(trackingID=trackingID)
-    if activityList.count():
-        return jsonify(
-            {
-                "code": 200,
-                "data": [activity.json() for activity in activityList]
-            }
-        )
+#### Receiving Activity Log ####
+monitorBindingKey = "#.order"
+
+def receiveActivity():
+    amqp_setup.check_setup()
+
+    queue_name = "Activity"
     
-    return jsonify(
-        {
-            "code": 404,
-            "data": {
-                "tracking_id": trackingID
-            },
-            "message": "No Activity Found."
-        }
-    ), 404
+    # set up a consumer and start to wait for coming messages
+    amqp_setup.channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+    amqp_setup.channel.start_consuming() # an implicit loop waiting to receive messages; 
+    #it doesn't exit by default. Use Ctrl+C in the command window to terminate it.
+
+def callback(channel, method, properties, body): # required signature for the callback; no return
+    print("\nReceived an activity log by " + __file__)
+    processActivity(json.loads(body))
+    print() # print a new line feed
+
+def processActivity(activity):
+    print("Recording an order log:")
+    print(activity)
+    data = activity["data"]
+
+    # Creating new row in db
+    if activity['code'] == 201:
+        activityr = Activity(trackingID=data['tracking_id'], deliveryStatus=data['delivery_status'], deliveryDesc=data['delivery_desc'])
+    
+    try:
+        db.session.add(activityr)
+        db.session.commit()
+    except Exception as e:
+        print("Error encountered: ", e)
+        print()
+
+
+#### Getting Activity Log ####
+
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL') or 'mysql+mysqlconnector://root@localhost:3306/camelcourier'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 299}
+
+# db = SQLAlchemy(app)
+
+# CORS(app)
+
+# class Activity(db.Model):
+#     __tablename__ = 'activity'
+
+#     activityID = db.Column(db.Integer, primary_key=True)
+#     trackingID = db.Column(db.Integer, nullable=False)
+#     timestamp = db.Column(db.DateTime, nullable=False, default=datetime.now,        onupdate=datetime.now)
+#     deliveryStatus = db.Column(db.String(30), nullable=False)
+#     deliveryDesc = db.Column(db.String(130), nullable=False)
+
+#     def json(self):
+#         dto = {
+#             'activity_id': self.activityID,
+#             'tracking_id': self.trackingID,
+#             'timestamp': self.timestamp,
+#             'delivery_status': self.deliveryStatus,
+#             'delivery_desc': self.deliveryDesc
+#         }
+
+#         return dto
+
+# @app.route("/activity/<string:trackingID>")
+# def find_activity_by_tracking(trackingID):
+#     activityList =  Activity.query.filter_by(trackingID=trackingID)
+#     if activityList.count():
+#         return jsonify(
+#             {
+#                 "code": 200,
+#                 "data": [activity.json() for activity in activityList]
+#             }
+#         )
+    
+#     return jsonify(
+#         {
+#             "code": 404,
+#             "data": {
+#                 "tracking_id": trackingID
+#             },
+#             "message": "No Activity Found."
+#         }
+#     ), 404
 
 if __name__ == '__main__':
     # print("This is flask for " + os.path.basename(__file__) + ": log activities ...")
---
+++ activity.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Mar 22, 2022 at 03:27 AM
+-- Server version: 8.0.21
+-- PHP Version: 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `camelcourier`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `activity`
+--
+
+DROP TABLE IF EXISTS `activity`;
+CREATE TABLE IF NOT EXISTS `activity` (
+  `activityID` int NOT NULL AUTO_INCREMENT,
+  `trackingID` int NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `deliveryStatus` varchar(30) NOT NULL,
+  `deliveryDesc` varchar(100) NOT NULL,
+  PRIMARY KEY (`activityID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `activity`
+--
+
+INSERT INTO `activity` (`activityID`, `trackingID`, `timestamp`, `deliveryStatus`, `deliveryDesc`) VALUES
+(1, 1337, '2022-03-19 20:45:24', 'Order Created!', 'The order has been created.'),
+(2, 42069, '2022-03-19 20:48:49', 'Order Created!', 'The order has been created.'),
+(3, 1337, '2022-03-20 12:09:57', 'Pickup', 'Order has been picked up by pickup driver');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
