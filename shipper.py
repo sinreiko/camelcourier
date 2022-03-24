43c794be67710ed2c02a8afbe9499768a0300aaf
w4ff13xp <90041643+w4ff13xp@users.noreply.github.com>
2022-03-24 17:09:04 +0800

    Update shipper.py

--- shipper.py
+++ shipper.py
@@ -9,7 +9,7 @@ from os import environ
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get(
-    'dbURL') or 'mysql+mysqlconnector://root:root@localhost:3306/order'
+    'dbURL') or 'mysql+mysqlconnector://root:root@localhost:3306/shipper'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
