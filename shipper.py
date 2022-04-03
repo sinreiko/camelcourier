ca873865a0b656e5edc18a887fd64f8e97b2d82d
w4ff13xp <90041643+w4ff13xp@users.noreply.github.com>
2022-04-03 14:08:01 +0800

    updated shipper.py with documentation

--- shipper.py
+++ shipper.py
@@ -1,24 +1,44 @@
+
+   
+# ===========================================
+
+#           I M P O R T S
+
+# ----------------
 import os
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-
 from datetime import datetime
 import json
 from os import environ
 
+# ===========================================
+
+#           D B   S E T   U P   A  N D   C U S T O M  C L A S S
+
+# ----------------
 app = Flask(__name__)
 # NOTE! main db name changed to camelcourier. Pls import the new sql called camelcourier!
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get(
     'dbURL') or 'mysql+mysqlconnector://is213@localhost:3306/camelDB'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
-
 CORS(app)
 
+# Define a custom class for each shipper record.
 
 class Shipper(db.Model):
+    '''
+        Takes in the following attributes:
+        - shipperID -> autoincrement string to serve as the primary key of the shipper in the db
+        - shipperName -> created when shipper inputs name during account sign up
+        - shipperAddress -> created when shipper inputs address during account sign up
+        - shipperPhone -> created when shipper inputs phone number during account sign up
+        - shipperEmail -> created when shipper inputs email during account sign up
+        - createdDate -> generated when shipper creates account
+        - modifiedDate -> generated when shipper modifies account
+    '''
     __tablename__ = 'shipper'
     shipperID = db.Column(db.Integer, primary_key=True)
     shipperName = db.Column(db.String(32), nullable=False)
@@ -47,6 +67,19 @@ class Shipper(db.Model):
                 "createdDate": self.createdDate,
                 "modifiedDate": self.modifiedDate}
 
+# ===========================================
+
+#           U T I L I T Y   F U N C T I O N S
+    # ---------
+        # get_all(): Retrieves all entries from the db and returns them in JSON format
+    # ---------
+
+# ----------------------------------
+
+    # ---------------[START: get_all]-------------------
+    # [TESTED] This url returns all shipper database info in json format
+    # This is used to troubleshoot and see if the data in db is ok
+    # Uncomment line below to test this function
 
 @app.route("/shipper")
 def get_all():
@@ -67,6 +100,21 @@ def get_all():
         }
     ), 404
 
+    # ----------------[END: get_all]------------------
+
+# ===========================================
+
+#           A P I   F U N C T I O N S
+    # -----------
+    # find_by_shipperID(): fulfills app route /shipper/<shipperID> to return information of a given shipper ID with method GET
+    # create_shipper(): fulfills app route /shipper to add a new shipper, generate a shipperID and assign a shipper with method POST
+    # update_order(): fulfills app route /order/update to update the pickupAddress of an entry given a trackingID with method PUT
+    # -----------
+
+# ----------------------------------
+
+    # -----------------[START: find_by_shipperID]-------------------
+# [TESTED] This url fulfills shipper checking.
 
 @app.route("/shipper/<string:shipperID>")
 def find_by_shipperID(shipperID):
@@ -88,8 +136,10 @@ def find_by_shipperID(shipperID):
         }
     ), 404
 
-# try to implement facebook unrestful api for shipper account sign in
+    # ---------------[END: find_by_shipperID]-------------------
 
+    # ----------------[START: create_shipper]------------------
+# [TESTED] This URL adds a new shipper into the db.
 
 @app.route("/shipper/<string:shipperID>", methods=['POST'])
 def create_shipper(shipperID):
@@ -109,7 +159,8 @@ def create_shipper(shipperID):
 
     try:
         db.session.add(shipper)
-        db.session.commit()
+        status = db.session.commit()
+        print(status)
     except:
         return jsonify(
             {
@@ -128,7 +179,11 @@ def create_shipper(shipperID):
         }
     ), 201
 
+    # ----------------[END: create_order]------------------
 
+# ===========================================
+
+# Alternative app call failsafe
 if __name__ == '__main__':
     print("This is flask for " +
           os.path.basename(__file__) + ": manage shippers ...")
