4241b20b07e198f2c06a2ddccb3532ab036def48
Pochienlin <pochien.lin.2019@socsc.smu.edu.sg>
2022-04-05 03:03:42 +0800

    droppointPHP
    drop point php now works in test environment. follows same port and path as python version.
    
    pending testing

--- droppointPHP.Dockerfile
+++ droppointPHP.Dockerfile
@@ -3,8 +3,11 @@ FROM php:7.3-apache
 #Install git and MySQL extensions for PHP
 
 RUN apt-get update && apt-get install -y git
-RUN docker-php-ext-install pdo pdo_mysql mysqli
+RUN docker-php-ext-install pdo pdo_mysql
+RUN docker-php-ext-install mysqli && docker-php-ext-enable mysqli
 RUN a2enmod rewrite
+RUN php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+
 COPY . /var/www/html/
+EXPOSE 5004/tcp
 EXPOSE 80/tcp
-EXPOSE 443/tcp
--- php/droppoint/api/config/database.php
+++ php/droppoint/api/config/database.php
@@ -3,17 +3,17 @@
 class Database {
 
     // specify your own database credentials
-    // private $host = getenv('MYSQL_DBHOST')? getenv('MYSQL_DBHOST') :"host.docker.internal";
-    // private $db_name =getenv('MYSQL_DBNAME')? getenv('MYSQL_DBNAME'):"camelDB";
-    // private $username =getenv('MYSQL_DBUSER')? getenv('MYSQL_DBUSER'):"is213";
-    // private $password =getenv('MYSQL_DBPASS')? getenv('MYSQL_DBPASS'):""; // MAMP "root", WAMP empty string
-    // private $port = getenv('MYSQL_DBPORT')? getenv('MYSQL_DBPORT'):3306; // Check in PHPMyAdmin for port number
     private $host = "host.docker.internal";
-    private $db_name ="camelDB";
+    private $db_name = "camelDB";
     private $username ="is213";
     private $password =""; // MAMP "root", WAMP empty string
-    private $port =3306; // Check in PHPMyAdmin for port number
-    public $conn;
+    private $port = 3306; // Check in PHPMyAdmin for port number
+    // private $host = "host.docker.internal";
+    // private $db_name ="camelDB";
+    // private $username ="is213";
+    // private $password =""; // MAMP "root", WAMP empty string
+    // private $port =9906; // Check in PHPMyAdmin for port number
+    // public $conn;
 
     // get the database connection
     public function getConnection() {
